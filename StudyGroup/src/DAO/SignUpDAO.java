b7be61553573f8d464ad6ea57136d30b32ffeba2
ally010314 <ally010314@naver.com>
2025-05-10 10:48:57 +0900

    sql 연결 설정

---
+++ StudyGroup/src/DAO/SignUpDAO.java
@@ -0,0 +1,43 @@
+package DAO;
+
+import java.sql.*;
+
+import main.AppMain;
+
+public class SignUpDAO {
+	
+
+	public boolean isDuplicateLoginId(String loginId) {
+        String sql = "SELECT 1 FROM Users WHERE login_id = ?";
+
+        try (PreparedStatement stmt = AppMain.conn.prepareStatement(sql)) {
+            stmt.setString(1, loginId);
+            try (ResultSet rs = stmt.executeQuery()) {
+                return rs.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public boolean registerUser(String loginId, String userName, String password) {
+        String sql = "INSERT INTO Users (login_id, user_name, password_hash) VALUES (?, ?, ?)";
+
+        try (PreparedStatement stmt = AppMain.conn.prepareStatement(sql)) {
+            stmt.setString(1, loginId);
+            stmt.setString(2, userName);
+            stmt.setString(3, password);
+
+            int rows = stmt.executeUpdate();
+            return rows > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+}
