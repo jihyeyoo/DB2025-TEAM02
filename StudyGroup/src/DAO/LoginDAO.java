643f60fa7fc27ef6425e1c1d471088559be24dd2
Hyewon Joo <joohw1004@gmail.com>
2025-05-16 20:12:56 +0900

    Login, SignUp, MyPage, ChargePoint 페이지 완료. MyStudyGroups, RefundInfo, UserInfo는 빈 페이지. LoginDAO 완료.

--- StudyGroup/src/DAO/LoginDAO.java
+++ StudyGroup/src/DAO/LoginDAO.java
@@ -1,5 +1,25 @@
 package DAO;
 
+import java.sql.*;
+
+import main.AppMain;
+
 public class LoginDAO {
 
+    public boolean checkLogin(String loginId, String password) {
+        String sql = "SELECT * FROM Users WHERE login_id = ? AND password_hash = ?";
+
+        try (PreparedStatement stmt = AppMain.conn.prepareStatement(sql)) {
+            stmt.setString(1, loginId);
+            stmt.setString(2, password);
+
+            ResultSet rs = stmt.executeQuery();
+            return rs.next(); // 있으면 로그인 성공
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false; // 예외 시 실패
+    }
 }
--- StudyGroup/src/main/main/AppMain.java
+++ StudyGroup/src/main/main/AppMain.java
@@ -1,30 +1,29 @@
 package main;
 
 import java.sql.*;
-
-import GUI.SignUp;
+import GUI.Login;
 
 public class AppMain {
     public static Connection conn;
 
     public static void main(String[] args) {
         String DRIVER = "com.mysql.cj.jdbc.Driver";
-        String DBURL = "jdbc:mysql://localhost:3306/db2025team02";
-        String DBID = "root"; //본인 sql user 이름 입력하세요
-        String DBPW = ""; //본인 sql pw 입력하세요
+        String DBURL = "jdbc:mysql://localhost:3306/DB2025Team02";
+        String DBID = "root"; // 본인 계정
+        String DBPW = "cindy1004"; // 본인 비밀번호
 
         try {
             Class.forName(DRIVER);
-
             conn = DriverManager.getConnection(DBURL, DBID, DBPW);
-            System.out.println("연결되었습니다.");
-            
-            new SignUp();
+            System.out.println("DB 연결되었습니다.");
+
+            new Login(); // 로그인 창부터 시작
 
         } catch (ClassNotFoundException e) {
             System.out.println("JDBC 드라이버를 찾을 수 없습니다.");
         } catch (SQLException e) {
             System.out.println("DB 연결 오류");
+            e.printStackTrace();
         }
     }
 }
