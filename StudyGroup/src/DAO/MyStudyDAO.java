eeee1d5efa453003eeb644e058608024e947b002
SeungHee-M <highee@ewhain.net>
2025-05-20 00:59:01 +0900

    status 인덱스 이용한 정렬기능 적용

--- StudyGroup/src/DAO/MyStudyDAO.java
+++ StudyGroup/src/DAO/MyStudyDAO.java
@@ -2,11 +2,10 @@ package DAO;
 
 import DTO.MyStudyDTO;
 import DTO.UserDTO;
-import main.AppMain;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import main.AppMain;
 
 public class MyStudyDAO {
 
@@ -54,21 +53,30 @@ public class MyStudyDAO {
     
     public List<MyStudyDTO> getMyStudies(UserDTO user) {
         List<MyStudyDTO> studyList = new ArrayList<>();
-        String sql = "SELECT sg.study_id, sg.name AS study_name, u.user_name AS leader_name, sg.start_date " +
-                     "FROM GroupMembers gm " +
-                     "JOIN StudyGroups sg ON gm.study_id = sg.study_id " +
-                     "JOIN Users u ON sg.leader_id = u.user_id " +
-                     "WHERE gm.user_id = ?";
+        String sql = "SELECT sg.study_id, sg.name AS study_name, u.user_name AS leader_name, sg.start_date, sg.status" +
+                    "FROM GroupMembers gm " +
+                    "JOIN StudyGroups sg ON gm.study_id = sg.study_id " +
+                    "JOIN Users u ON sg.leader_id = u.user_id " +
+                    "WHERE gm.user_id = ? " +
+                    "ORDER BY " +
+                    "  CASE sg.status " +
+                    "    WHEN '진행중' THEN 0 " +
+                    "    WHEN '모집중' THEN 1 " +
+                    "    WHEN '종료' THEN 2 " +
+                    "    ELSE 3 " +
+                    "  END, " +
+                    "  sg.start_date DESC"; //status 인덱스를 사용해 진행중, 모집중, 종료 순서로 출력되도록 수정
 
         try (PreparedStatement pstmt = AppMain.conn.prepareStatement(sql)) {
-            pstmt.setInt(1, user.getUserId());  // userId 바로 사용
+            pstmt.setInt(1, user.getUserId());
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
                 MyStudyDTO dto = new MyStudyDTO(
                         rs.getInt("study_id"),
                         rs.getString("study_name"),
                         rs.getString("leader_name"),
-                        rs.getDate("start_date")
+                        rs.getDate("start_date"),
+                        rs.getString("status") //statuc 컬럼 추가
                 );
                 studyList.add(dto);
             }
@@ -78,6 +86,7 @@ public class MyStudyDAO {
 
         return studyList;
     }
+
 
     public boolean withdrawFromStudy(int studyId, UserDTO user) {
         String sql = "DELETE FROM GroupMembers WHERE study_id = ? AND user_id = ?";
